87c15370fed3c6c4640e6452025d46973fb51f98
Relgrand Giresse MOUNGUEMBO MABELE <relgrandgiressemounguembomabele@MacBook-Pro-de-Relgrand.local>
2024-01-16 16:01:00 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+/*import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';*/
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [HomeComponent, /*CommonModule, RouterOutlet*/],
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+`,
+  //templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'first-app';
+  title = 'homes';
 }
